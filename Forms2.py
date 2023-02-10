7674c9b9fb32e9fe0d41b014105b97c034fd9a7f
jaithakur19 <jaithakur1905@gmail.com>
2023-02-10 15:34:42 +0800

    Update createFeedback.html and updateFeedback.html

--- Forms2.py
+++ Forms2.py
@@ -3,17 +3,24 @@ from wtforms.fields import EmailField, DateField, StringField, TextAreaField, Ra
 from datetime import date
 
 class CreateFeedbackForm(Form):
-    date = DateField('Date', [validators.DataRequired()], default=date.today())
+    date = DateField('Date', [validators.DataRequired()], default=date.today(), render_kw={'readonly': True})
     name = StringField('Name', [validators.DataRequired(), validators.Length(min=2, max=150)])
-    email = EmailField('Email', [validators.DataRequired()])
+    email = StringField('Email')
     typeqn = RadioField('Type of Feedback', choices=[('C', 'Consultation'), ('M', 'Medication'), ('O', 'Others')])
-    qn1 = TextAreaField('How was your experience with us?', [validators.DataRequired()])
+    qn1 = TextAreaField('How was your experience with us?')
     qn2 = TextAreaField('How could we improve?', [validators.DataRequired()])
 
     def validate_date(form, field):
         today = date.today()
         if field.data.strftime('%Y-%m-%d') < today.strftime('%Y-%m-%d'):
             raise ValidationError("Select a date that's not in the past")
+
+
+    def validate_email(form, field):
+        if '@' not in field.data:
+            raise ValidationError("Please make sure your email contains '@'")
+        elif '.com' not in field.data:
+            raise ValidationError("Please make sure your email contains '.com' at the end")
 
 
 
